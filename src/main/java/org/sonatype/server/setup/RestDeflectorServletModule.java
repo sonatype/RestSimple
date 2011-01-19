ae9725915ce8f87bbbc0ae1afef6cacfc77623c4
jfarcand <jfarcand@apache.org>
2011-01-19 13:58:33 -0500

    Proper provider

--- src/main/java/org/sonatype/server/setup/RestDeflectorServletModule.java
+++ src/main/java/org/sonatype/server/setup/RestDeflectorServletModule.java
@@ -16,16 +16,16 @@ public class RestDeflectorServletModule extends ServletModule {
 
         bind(ServiceEntity.class).to(AddressBookServiceEntity.class);
 
-        // First, bind our Service Provider
-        bind(ServiceDefinitionProvider.class).to(JAXRSServiceDefinitionProvider.class);
-
-        bind(ServiceDefinition.class).toProvider(ServiceDefinitionProvider.class);
-
-        // Next, Define our Resource Generator
-        bind(ServiceDefinitionGenerator.class).toInstance(new JAXRSServiceDefinitionGenerator(binder()));
-
-        //Finally, bind our implementation
-        bind(ServiceDefinitionJAXRSTest.class).asEagerSingleton();
+//        // First, bind our Service Provider
+//        bind(ServiceDefinitionProvider.class).to(JAXRSServiceDefinitionProvider.class);
+//
+//        bind(ServiceDefinition.class).toProvider(ServiceDefinitionProvider.class);
+//
+//        // Next, Define our Resource Generator
+//        bind(ServiceDefinitionGenerator.class).toInstance(new JAXRSServiceDefinitionGenerator(binder()));
+//
+//        //Finally, bind our implementation
+//        bind(ServiceDefinitionJAXRSTest.class).asEagerSingleton();
 
         install(new JaxrsModule());
 
