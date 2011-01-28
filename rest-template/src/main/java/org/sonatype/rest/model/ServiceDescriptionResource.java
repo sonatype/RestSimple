50e645b3c67a879d172e9602e901b7163fecfe06
jfarcand <jfarcand@apache.org>
2011-01-28 18:07:28 -0500

    Add a template module for debugging purpose. This is how resource looks like when they gets generated

---
+++ rest-template/src/main/java/org/sonatype/rest/model/ServiceDescriptionResource.java
@@ -0,0 +1,180 @@
+package org.sonatype.rest.model;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonatype.rest.api.PostServiceHandler;
+import org.sonatype.rest.api.ServiceEntity;
+import org.sonatype.rest.api.ServiceHandler;
+import org.sonatype.rest.api.ServiceHandlerMediaType;
+import org.sonatype.rest.spi.ServiceHandlerMapper;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+
+/**
+ * This is what we want to generate automatically using ASM.
+ * <p/>
+ * NOTE: This class is not used, but we do instead generate some form ot it based on {@link org.sonatype.rest.api.ServiceDefinition}
+ */
+@Path("/bar/{method}/{id}/")
+@Produces("application/org.sonatype.rest.tests.addressBook+json")
+@Consumes("application/org.sonatype.rest.tests.addressBook+json")
+public class ServiceDescriptionResource {
+
+    private Logger logger = LoggerFactory.getLogger(ServiceDescriptionResource.class);
+
+    @Inject
+    ServiceEntity serviceEntity;
+
+    @Inject
+    ServiceHandlerMapper mapper;
+
+    @Inject
+    ServiceHandlerMediaType producer;
+
+    @GET
+    public ServiceHandlerMediaType get(@PathParam("method") String service, @PathParam("id") String value) {
+        logger.debug("HTTP GET: Generated Resource invocation for method {} with id {}", service, value);
+        Object response = createResponse("get", service, value, null);
+        return producer.visit(response);
+    }
+
+    @HEAD
+    public Response head(@PathParam("method") String service, @PathParam("id") String value) {
+        logger.debug("HTTP HEAD: Generated Resource invocation for method {} with id {}", service, value);
+        Object response = createResponse("head", service, value, null);
+        return Response.ok().build();
+    }
+
+    @PUT
+    public Response put(@PathParam("method") String service, @PathParam("id") String value) {
+        logger.debug("HTTP PUT: Generated Resource invocation for method {} with id {}", service, value);
+        URI location = UriBuilder.fromResource(getClass()).build(new String[]{"", ""});
+        Object response = createResponse("put", service, value, null);
+        return Response.created(location).entity(response).build();
+    }
+
+//    @POST
+//    @Consumes("application/x-www-form-urlencoded")
+//    public Response post(@PathParam("method") String service, @PathParam("id") String value, MultivaluedMap<String, String> formParams ) {
+//        logger.debug("HTTP POST: Generated Resource invocation for method {} with id {} and update {}", service, value);
+//        Object response = createResponse("post", service, value, formParams);
+//        if (response == null) {
+//            return Response.status(Response.Status.NO_CONTENT).build();
+//        } else {
+//            return Response.ok(response).build();
+//        }
+//    }
+    
+    @POST
+    public Response postJson(@PathParam("method") String service, @PathParam("id") String value, String body) {
+        logger.debug("HTTP POST: Generated Resource invocation for method {} with id {} and update {}", service, value);
+        Object response = delegate("post", service, value, body);
+        if (response == null) {
+            return Response.status(Response.Status.NO_CONTENT).build();
+        } else {
+            return Response.ok(response).build();
+        }
+    }
+
+    @DELETE
+    public Response delete(@PathParam("method") String service, @PathParam("id") String value) {
+        logger.debug("HTTP DELETE: Generated Resource invocation for method {} with id {}", service, value);
+        Object response = createResponse("delete", service, value, null);
+        return Response.ok(response).build();
+    }
+
+    private Object createResponse(String methodName, String service, String value, MultivaluedMap<String, String> formParams) {
+        ServiceHandler serviceHandler = mapper.map(service);
+        if (serviceHandler == null) {
+            throw new WebApplicationException(new IllegalStateException("No ServiceHandler defined for service " + service));
+        }
+
+        if (!serviceHandler.getHttpMethod().name().equalsIgnoreCase(methodName)) {
+            throw new WebApplicationException(Response.status(405).entity("Method not allowed").build());
+        }
+
+        String methodString = serviceHandler.getServiceEntityMethod();
+        Class[] classes = new Class[1];
+        classes[0] = String.class;
+        Object[] objects = new Object[1];
+        objects[0] = value;
+        try {
+            Object response;
+            if (PostServiceHandler.class.isAssignableFrom(serviceHandler.getClass())) {
+
+                if (formParams == null) {
+                    throw new WebApplicationException(Response.status(500).entity("No form params").build());
+                }
+                                
+                classes = new Class[formParams.size() + 1];
+                objects = new Object[formParams.size() + 1];
+
+                classes[0] = String.class;
+                objects[0] = value;
+
+                int i = 1;
+                List<String> values = null;
+                for(Map.Entry<String, List<String>> entry: formParams.entrySet()) {
+                    values = entry.getValue();
+                    classes[i] = String.class;
+                    objects[i++] = values.get(0);
+                }
+            }
+            Method method = serviceEntity.getClass().getMethod(methodString, classes);
+            response = method.invoke(serviceEntity, objects);
+
+            return response;
+        } catch (Throwable e) {
+            logger.error("createResponse", e);
+            throw new WebApplicationException(e);
+        }
+    }
+
+    private Object delegate(String methodName, String service, String value, String body) {
+        ServiceHandler serviceHandler = mapper.map(service);
+        if (serviceHandler == null) {
+            throw new WebApplicationException(new IllegalStateException("No ServiceHandler defined for service " + service));
+        }
+
+        if (!serviceHandler.getHttpMethod().name().equalsIgnoreCase(methodName)) {
+            throw new WebApplicationException(Response.status(405).entity("Method not allowed").build());
+        }
+
+        String methodString = serviceHandler.getServiceEntityMethod();
+        Class[] classes = new Class[2];
+        classes[0] = String.class;
+        classes[1] = String.class;
+        Object[] objects = new Object[2];
+        objects[0] = value;
+        objects[1] = body;        
+        try {
+            Method method = serviceEntity.getClass().getMethod(methodString, classes);
+            return method.invoke(serviceEntity, objects);
+        } catch (Throwable e) {
+            logger.error("createResponse", e);
+            throw new WebApplicationException(e);
+        }
+    }
+}
---
+++ rest-template/src/main/java/org/sonatype/rest/model/SitebricksResource.java
@@ -0,0 +1,184 @@
+package org.sonatype.rest.model;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.google.sitebricks.At;
+import com.google.sitebricks.client.transport.Json;
+import com.google.sitebricks.client.transport.Text;
+import com.google.sitebricks.headless.Reply;
+import com.google.sitebricks.headless.Request;
+import com.google.sitebricks.http.Delete;
+import com.google.sitebricks.http.Get;
+import com.google.sitebricks.http.Post;
+import com.google.sitebricks.http.Put;
+import com.google.sitebricks.http.negotiate.Accept;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonatype.rest.api.PostServiceHandler;
+import org.sonatype.rest.api.ServiceEntity;
+import org.sonatype.rest.api.ServiceHandler;
+import org.sonatype.rest.api.ServiceHandlerMediaType;
+import org.sonatype.rest.spi.ServiceHandlerMapper;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+
+@At("/:method/:id")
+public final class SitebricksResource {
+
+    private String update;
+
+    private Logger logger = LoggerFactory.getLogger(SitebricksResource.class);
+
+    @Inject
+    ServiceEntity serviceEntity;
+
+    @Inject
+    ServiceHandlerMapper mapper;
+
+    @Inject
+    ServiceHandlerMediaType producer;
+
+    @Get
+    @Accept("application/org.sonatype.rest.tests.addressBook+json")
+    public Reply<?> get(@Named("method") String service, @Named("id") String value) {
+        logger.debug("HTTP GET: Generated Resource invocation for method {} with id {}", service, value);
+        Object response = createResponse("get", service, value);
+
+        if (Reply.class.isAssignableFrom(response.getClass())) {
+            return Reply.class.cast(response);
+        }
+
+        return Reply.with(producer.visit(response)).as(Json.class);
+    }
+
+    @Put
+    @Accept("application/org.sonatype.rest.tests.addressBook+json")
+    public Reply<?> put(@Named("method") String service, @Named("id") String value) {
+        logger.debug("HTTP PUT: Generated Resource invocation for method {} with id {}", service, value);
+        Object response = createResponse("put", service, value);
+
+        if (Reply.class.isAssignableFrom(response.getClass())) {
+            return Reply.class.cast(response);
+        }
+
+        return Reply.with(response.toString()).status(201);
+    }
+
+//    @Post @Accept("application/org.sonatype.rest.tests.addressBook+json")
+//    public Reply<?> post(@Named("method") String service, @Named("id") String value) {
+//        logger.debug("HTTP POST: Generated Resource invocation for method {} with id {} and update {}", service, value);
+//        Object response = createResponse("post", service, value);
+//
+//        if (Reply.class.isAssignableFrom(response.getClass())) {
+//            return Reply.class.cast(response);
+//        }
+//
+//        if (response == null) {
+//            return Reply.with("").noContent();
+//        } else {
+//            return Reply.with(response.toString());
+//        }
+//    }
+
+    @Post
+    @Accept("application/org.sonatype.rest.tests.addressBook+json")
+    public Reply<?> post(@Named("method") String service, @Named("id") String value, Request request) {
+        logger.debug("HTTP POST: Generated Resource invocation for method {} with id {} and update {}", service, value);
+        String body = request.read(String.class).as(Text.class);
+        Object response = delegate("post", service, value, body);
+
+        if (Reply.class.isAssignableFrom(response.getClass())) {
+            return Reply.class.cast(response);
+        }
+
+        if (response == null) {
+            return Reply.with("").noContent();
+        } else {
+            return Reply.with(response.toString());
+        }
+    }
+
+    @Delete
+    @Accept("application/org.sonatype.rest.tests.addressBook+json")
+    public Reply<?> delete(@Named("method") String service, @Named("id") String value) {
+        logger.debug("HTTP DELETE: Generated Resource invocation for method {} with id {}", service, value);
+        Object response = createResponse("delete", service, value);
+        return Reply.with(response.toString());
+    }
+
+    private Object createResponse(String methodName, String service, String value) {
+        ServiceHandler serviceHandler = mapper.map(service);
+        if (serviceHandler == null) {
+            return Reply.with("No ServiceHandler defined for service " + service).error();
+        }
+
+        if (!serviceHandler.getHttpMethod().name().equalsIgnoreCase(methodName)) {
+            return Reply.with("Method not allowed").status(405);
+        }
+
+        String methodString = serviceHandler.getServiceEntityMethod();
+        Class[] classes = new Class[1];
+        classes[0] = String.class;
+        Object[] objects = new Object[1];
+        objects[0] = value;
+        try {
+            Object response;
+            if (PostServiceHandler.class.isAssignableFrom(serviceHandler.getClass())) {
+                List<String> formParams = PostServiceHandler.class.cast(serviceHandler).formParams();
+                classes = new Class[formParams.size() + 1];
+                objects = new Object[formParams.size() + 1];
+
+                classes[0] = String.class;
+                objects[0] = value;
+
+                int i = 1;
+                for (String formParam : formParams) {
+                    classes[i] = String.class;
+                    objects[i++] = (String) this.getClass().getDeclaredField(formParam).get(this);
+                    logger.info("Getting generated value for form param {} : {}", formParam, this.getClass().getDeclaredField(formParam).get(this));
+                }
+            }
+            Method method = serviceEntity.getClass().getMethod(methodString, classes);
+            response = method.invoke(serviceEntity, objects);
+
+            return response;
+        } catch (Throwable e) {
+            logger.error("createResponse", e);
+            return Reply.with(e).error();
+        }
+    }
+
+    public void setUpdate(String update) {
+        this.update = update;
+    }
+
+    private Object delegate(String methodName, String service, String value, String body) {
+        ServiceHandler serviceHandler = mapper.map(service);
+        if (serviceHandler == null) {
+            return Reply.with("No ServiceHandler defined for service " + service).error();
+        }
+
+        if (!serviceHandler.getHttpMethod().name().equalsIgnoreCase(methodName)) {
+            return Reply.with("Method not allowed").status(405);
+        }
+
+        String methodString = serviceHandler.getServiceEntityMethod();
+        Class[] classes = new Class[2];
+        classes[0] = String.class;
+        classes[1] = String.class;
+        Object[] objects = new Object[2];
+        objects[0] = value;
+        objects[1] = body;
+        try {
+            Method method = serviceEntity.getClass().getMethod(methodString, classes);
+            return method.invoke(serviceEntity, objects);
+        } catch (Throwable e) {
+            logger.error("delegate", e);
+            return Reply.with(e).error();
+        }
+    }
+}
+
+
