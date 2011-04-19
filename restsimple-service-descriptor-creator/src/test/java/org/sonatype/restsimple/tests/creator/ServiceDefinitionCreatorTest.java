bb13d511af531e8e9e9189b7d3056af1854b6108
jfarcand <jfarcand@apache.org>
2011-04-18 21:48:37 -0400

    Fix the licence

--- restsimple-service-descriptor-creator/src/test/java/org/sonatype/restsimple/tests/creator/ServiceDefinitionCreatorTest.java
+++ restsimple-service-descriptor-creator/src/test/java/org/sonatype/restsimple/tests/creator/ServiceDefinitionCreatorTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses.
+ *******************************************************************************/
 package org.sonatype.restsimple.tests.creator;
 
 import org.slf4j.Logger;
@@ -21,17 +32,6 @@ import org.testng.annotations.Test;
 
 import java.net.URI;
 import java.util.Collection;
-/*******************************************************************************
- * Copyright (c) 2010-2011 Sonatype, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Apache License v2.0 which accompanies this distribution.
- * The Eclipse Public License is available at
- *   http://www.eclipse.org/legal/epl-v10.html
- * The Apache License v2.0 is available at
- *   http://www.apache.org/licenses/LICENSE-2.0.html
- * You may elect to redistribute this code under either of these licenses.
- *******************************************************************************/
 import static org.testng.AssertJUnit.assertNotNull;
 
 public class ServiceDefinitionCreatorTest {
