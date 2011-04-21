7fe1013e067fd12639ea0ae19f892181ebae8579
jfarcand <jfarcand@apache.org>
2011-04-21 14:58:25 -0400

    First drop for the ServiceDefinition HTML generation a la enunciate

---
+++ restsimple-templating/src/main/java/org/sonatype/restsimple/templating/HtmlTemplateGenerator.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses.
+ *******************************************************************************/
+package org.sonatype.restsimple.templating;
+
+import org.sonatype.restsimple.api.ServiceDefinition;
+import org.sonatype.sisu.template.Templater;
+import org.sonatype.sisu.template.loader.FileTemplateLoader;
+
+import java.io.File;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Simple Velocity template generator who generates HTML view of a ServiceDefinition.
+ */
+public class HtmlTemplateGenerator implements TemplateGenerator {
+
+    private final Map<String, Object> context = new HashMap<String, Object>();
+
+    private final Templater templater;
+
+    public HtmlTemplateGenerator(Templater templater) {
+        this.templater = templater;
+    }
+
+    @Override
+    public String generateTemplate(ServiceDefinition serviceDefinition) throws Exception {
+        ClassLoader cl = getClass().getClassLoader();
+        URL sdUrl = cl.getResource("templates/ServiceDefinitions.vm");
+        File template = new File(sdUrl.toURI());
+
+        context.put("sd", serviceDefinition);
+
+        Writer writer = new StringWriter();
+        templater.setTemplateLoader( new FileTemplateLoader());
+        templater.renderTemplate( template.getAbsolutePath(), context, writer );
+        return writer.toString();
+    }
+
+}
---
+++ restsimple-templating/src/test/java/org/sonatype/restsimple/tests/templating/ServiceDefinitionTemplateTest.java
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses.
+ *******************************************************************************/
+package org.sonatype.restsimple.tests.templating;
+
+import org.sonatype.restsimple.api.Action;
+import org.sonatype.restsimple.api.DefaultServiceDefinition;
+import org.sonatype.restsimple.api.DeleteServiceHandler;
+import org.sonatype.restsimple.api.GetServiceHandler;
+import org.sonatype.restsimple.api.MediaType;
+import org.sonatype.restsimple.api.PostServiceHandler;
+import org.sonatype.restsimple.api.PutServiceHandler;
+import org.sonatype.restsimple.api.ServiceDefinition;
+import org.sonatype.restsimple.common.test.addressbook.AddressBookAction;
+import org.sonatype.restsimple.templating.HtmlTemplateGenerator;
+import org.sonatype.sisu.template.VelocityTemplater;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class ServiceDefinitionTemplateTest {
+
+    private final static String SD = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n" +
+            "        \"http://www.w3.org/TR/html4/loose.dtd\">\n" +
+            "<html>\n" +
+            "<head>\n" +
+            "    <title>ServiceDefinitionTemplate</title>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "<h1>ServiceDefinition</h1>\n" +
+            "\n" +
+            "<div>\n" +
+            "    <p>\n" +
+            "        ROOT URI: /serviceDefinition\n" +
+            "    </p>\n" +
+            "\n" +
+            "    <p>\n" +
+            "        Consuming:\n" +
+            "    <ul>\n" +
+            "            </ul>\n" +
+            "    </p>\n" +
+            "    <p>\n" +
+            "        Producing:\n" +
+            "    <ul>\n" +
+            "            </ul>\n" +
+            "    </p>\n" +
+            "\n" +
+            "</div>\n" +
+            "        <h2> ServiceHandler </h2>\n" +
+            "\n" +
+            "    <div>\n" +
+            "        <p>\n" +
+            "            URI: /serviceDefinition/createAddressBook/:ad\n" +
+            "        </p>\n" +
+            "\n" +
+            "        <p>\n" +
+            "            Method: PUT\n" +
+            "        </p>\n" +
+            "\n" +
+            "                    <p>\n" +
+            "            Consuming: MediaType{type='application', subType='vnd.org.sonatype.rest+json'}\n" +
+            "            </p>\n" +
+            "        \n" +
+            "        <p>\n" +
+            "            Producing:\n" +
+            "        <ul>\n" +
+            "                    </ul>\n" +
+            "        </p>\n" +
+            "        <p>\n" +
+            "            Action: AddressBookAction\n" +
+            "        </p>\n" +
+            "    </div>\n" +
+            "        <h2> ServiceHandler </h2>\n" +
+            "\n" +
+            "    <div>\n" +
+            "        <p>\n" +
+            "            URI: /serviceDefinition/getAddressBook/:ad\n" +
+            "        </p>\n" +
+            "\n" +
+            "        <p>\n" +
+            "            Method: GET\n" +
+            "        </p>\n" +
+            "\n" +
+            "                    <p>\n" +
+            "            Consuming: MediaType{type='application', subType='vnd.org.sonatype.rest+json'}\n" +
+            "            </p>\n" +
+            "        \n" +
+            "        <p>\n" +
+            "            Producing:\n" +
+            "        <ul>\n" +
+            "                    </ul>\n" +
+            "        </p>\n" +
+            "        <p>\n" +
+            "            Action: AddressBookAction\n" +
+            "        </p>\n" +
+            "    </div>\n" +
+            "        <h2> ServiceHandler </h2>\n" +
+            "\n" +
+            "    <div>\n" +
+            "        <p>\n" +
+            "            URI: /serviceDefinition/updateAddressBook/:ad\n" +
+            "        </p>\n" +
+            "\n" +
+            "        <p>\n" +
+            "            Method: POST\n" +
+            "        </p>\n" +
+            "\n" +
+            "                    <p>\n" +
+            "            Consuming: MediaType{type='application', subType='vnd.org.sonatype.rest+json'}\n" +
+            "            </p>\n" +
+            "        \n" +
+            "        <p>\n" +
+            "            Producing:\n" +
+            "        <ul>\n" +
+            "                    </ul>\n" +
+            "        </p>\n" +
+            "        <p>\n" +
+            "            Action: AddressBookAction\n" +
+            "        </p>\n" +
+            "    </div>\n" +
+            "        <h2> ServiceHandler </h2>\n" +
+            "\n" +
+            "    <div>\n" +
+            "        <p>\n" +
+            "            URI: /serviceDefinition/deleteAddressBook/:ad\n" +
+            "        </p>\n" +
+            "\n" +
+            "        <p>\n" +
+            "            Method: DELETE\n" +
+            "        </p>\n" +
+            "\n" +
+            "                    <p>\n" +
+            "            Consuming: MediaType{type='application', subType='vnd.org.sonatype.rest+json'}\n" +
+            "            </p>\n" +
+            "        \n" +
+            "        <p>\n" +
+            "            Producing:\n" +
+            "        <ul>\n" +
+            "                    </ul>\n" +
+            "        </p>\n" +
+            "        <p>\n" +
+            "            Action: AddressBookAction\n" +
+            "        </p>\n" +
+            "    </div>\n" +
+            "    </body>\n" +
+            "</html>";
+
+    @Test
+    public void basicServiceDefinitionTest() throws Exception {
+        Action action = new AddressBookAction();
+        PostServiceHandler postServiceHandler = new PostServiceHandler("/updateAddressBook/:ad", action);
+        postServiceHandler.addFormParam("update");
+        postServiceHandler.addFormParam("update2");
+        
+        ServiceDefinition serviceDefinition = new DefaultServiceDefinition();
+        serviceDefinition
+                .withPath("/serviceDefinition")
+                .producing(new MediaType(AddressBookAction.APPLICATION, AddressBookAction.JSON))
+                .producing(new MediaType(AddressBookAction.APPLICATION, AddressBookAction.XML))
+                .consuming(new MediaType(AddressBookAction.APPLICATION, AddressBookAction.JSON))
+                .withHandler(new PutServiceHandler("/createAddressBook/:ad", action))
+                .withHandler(new GetServiceHandler("/getAddressBook/:ad", action))
+                .withHandler(postServiceHandler)
+                .withHandler(new DeleteServiceHandler("/deleteAddressBook/:ad", action));
+
+
+        HtmlTemplateGenerator generator = new HtmlTemplateGenerator(new VelocityTemplater());
+        String sdString = generator.generateTemplate(serviceDefinition);        
+        assertEquals(sdString, SD);
+    }
+}
