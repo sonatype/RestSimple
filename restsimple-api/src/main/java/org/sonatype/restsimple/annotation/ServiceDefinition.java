7995399adc3d332a160778adaedf595559f02263
jfarcand <jfarcand@apache.org>
2011-06-10 12:50:49 -0400

    Add an annotation that can be used to mark ServiceDefinition when scanning for them

---
+++ restsimple-api/src/main/java/org/sonatype/restsimple/annotation/ServiceDefinition.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses.
+ *******************************************************************************/
+package org.sonatype.restsimple.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A maker annotation that can be used when annotating {@link org.sonatype.restsimple.api.ServiceDefinition} or POJO
+ * used with the service-decriptor-creator module.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ServiceDefinition {
+
+    public String name() default "";
+
+}
